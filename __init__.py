d1654c9e321768061703d40012a8b15bee743520
Hyungseok Ko <kohs102@gmail.com>
2025-06-27 00:37:37 +0900

    Added random weight feature

--- __init__.py
+++ __init__.py
@@ -53,19 +53,32 @@ class PPWCReplace:
 
     def sample_and_replace(self, input, replace, wildcard_list, seed):
         wc_list = wildcard_list.split("\n")
+
         normalized = []
+        no_weights = []
+
         for wc in wc_list:
             wc = wc.strip()
+            weight = 1
             if len(wc) > 0:
                 if wc.startswith("#"):
                     continue
+
+                if wc.startswith("|"):
+                    idx_weight_end = wc[1:].find("|")
+                    weight_str = wc[1:idx_weight_end+1]
+                    weight = float(weight_str)
+                    wc = wc[idx_weight_end+2:].strip()
+
                 if wc == "NOPROMPT":
                     normalized.append(None)
+                    no_weights.append(weight)
                 else:
                     normalized.append(wc)
+                    no_weights.append(weight)
 
         random.seed(seed)
-        choice = random.choice(normalized)
+        choice = random.choices(normalized, weights=no_weights, k=1)[0]
 
         pos = input.find(replace)
         if pos < 0:
@@ -83,65 +96,8 @@ class PPWCReplace:
             else:
                 return (f"{prefix}, {choice}, {postfix}",)
 
-
-class PPWCBlock:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "prefix": ("STRING", {"forceInput": True}),
-                "wildcard_list": (
-                    "STRING",
-                    {
-                        "multiline": True,
-                        "dynamicPrompts": False,
-                        "tooltip": "Enter a prompt using wildcard syntax.",
-                    },
-                ),
-                "seed": (
-                    "INT",
-                    {
-                        "default": 0,
-                        "min": 0,
-                        "max": 0xFFFFFFFFFFFFFFFF,
-                        "tooltip": "Determines the random seed to be used for wildcard processing.",
-                    },
-                ),
-            },
-        }
-
-    CATEGORY = "HSKOWildcard"
-    DESCRIPTION = "Description here."
-
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("output",)
-    FUNCTION = "sample_and_add"
-
-    def sample_and_add(self, prefix, wildcard_list, seed):
-        wc_list = wildcard_list.split("\n")
-        normalized = []
-        for wc in wc_list:
-            wc = wc.strip()
-            if len(wc) > 0:
-                if wc == "NOPROMPT":
-                    normalized.append(None)
-                else:
-                    normalized.append(wc)
-
-        random.seed(seed)
-        choice = random.choice(normalized)
-        if choice is None:
-            return (prefix,)
-        else:
-            return (prefix + ", " + choice,)
-
-
-NODE_CLASS_MAPPINGS = {"PPWCBlock": PPWCBlock, "PPWCReplace": PPWCReplace}
+NODE_CLASS_MAPPINGS = {"PPWCReplace": PPWCReplace}
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PPWCBlock": "Wildcard block",
     "PPWCReplace": "Wildcard replace",
 }
